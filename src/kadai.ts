4a483458040e44ce05b99b2c25b9c4d123b8bb34
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-02-18 13:41:39 +0900

    first

---
+++ src/kadai.ts
@@ -0,0 +1,50 @@
+class KadaiEntry {
+  kadaiID: string;
+  assignmentTitle: string;
+  assignmentDetail?: string;
+  dueDateTimestamp: number; // POSIX time
+  isFinished: boolean;
+
+  constructor(
+    kadaiID: string,
+    assignmentTitle: string,
+    dueDateTimestamp: number,
+    isFinished: boolean,
+    assignmentDetail?: string
+  ) {
+    this.kadaiID = kadaiID;
+    this.assignmentTitle = assignmentTitle;
+    this.assignmentDetail = assignmentDetail;
+    this.dueDateTimestamp = dueDateTimestamp;
+    this.isFinished = isFinished;
+  }
+}
+
+class Kadai {
+  lectureID: string;
+  lectureName: string;
+  kadaiEntries: Array<KadaiEntry>;
+  isRead: boolean;
+
+  constructor(
+    lectureID: string,
+    lectureName: string,
+    kadaiEntries: Array<KadaiEntry>,
+    isRead: boolean
+  ) {
+    this.lectureID = lectureID;
+    this.lectureName = lectureName;
+    this.kadaiEntries = kadaiEntries;
+    this.isRead = isRead;
+  }
+
+  get closestDueDateTimestamp(): number {
+    let max = 0;
+    for (const entry of this.kadaiEntries) {
+      if (max < entry.dueDateTimestamp) {
+        max = entry.dueDateTimestamp;
+      }
+    }
+    return max;
+  }
+}
