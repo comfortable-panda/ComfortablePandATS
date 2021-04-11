3b79b61f3aa4280999260ebd044aae271966662b
das08 <takeda5955@gmail.com>
2021-04-11 13:13:51 +0900

    返り値の型修正

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,24 @@
+import { LectureInfo } from "./kadai";
+import { DueGroupDom } from "./dom";
+import lectureName = DueGroupDom.lectureName;
+
 export const nowTime = new Date().getTime();
 
-function getDaysUntil(dt1: number, dt2: number) {
+function getDaysUntil(dt1: number, dt2: number): number {
   let diff = (dt2 - dt1) / 1000;
   diff /= 3600 * 24;
   if (diff < 0) diff = 9999;
   return diff;
 }
 
-function getTimeRemain(_remainTime: number) {
+function getTimeRemain(_remainTime: number): [number, number, number] {
   const day = Math.floor(_remainTime / (3600 * 24));
   const hours = Math.floor((_remainTime - day * 3600 * 24) / 3600);
   const minutes = Math.floor((_remainTime - (day * 3600 * 24 + hours * 3600)) / 60);
   return [day, hours, minutes];
 }
 
-function createElem(tag: any, dict?: { [key: string]: any }) {
+function createElem(tag: any, dict?: { [key: string]: any }): any {
   const elem = document.createElement(tag);
   for (const key in dict) {
     // @ts-ignore
@@ -30,5 +34,13 @@ function appendChildAll(to: HTMLElement, arr: Array<any>): HTMLElement {
   return to;
 }
 
+function createLectureIDMap(lectureIDList: Array<LectureInfo>): Map<string, string>{
+  const lectureIDMap = new Map<string, string>();
+  for (const lec of lectureIDList) {
+    lectureIDMap.set(lec.lectureID, lec.lectureName);
+  }
+  return lectureIDMap;
+}
 
-export { getDaysUntil, getTimeRemain, createElem, appendChildAll}
+
+export { getDaysUntil, getTimeRemain, createElem, appendChildAll, createLectureIDMap }
