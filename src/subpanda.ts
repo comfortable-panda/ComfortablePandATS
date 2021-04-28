58692932b279672614d00213549f395b28223bf9
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-04-28 17:30:47 +0900

    json dump

--- src/subpanda.ts
+++ src/subpanda.ts
@@ -1,14 +1,20 @@
+import { Kadai } from './kadai';
 import {loadFromStorage} from './storage'
 
-function dumpCache() {
+async function dumpCache() {
     const subpandaRoot = document.querySelector("#subpanda");
     if (subpandaRoot == null) return;
 
-    loadFromStorage("TSkadaiList").then(j => {
-        subpandaRoot.innerHTML = j;
-    });
+    const kadais = await loadFromStorage("TSkadaiList") as Array<Kadai>;
+    let textToShow = "";
+    for (const kadai of kadais) {
+        textToShow += kadai.lectureID + "\n";
+        for (const entry of kadai.kadaiEntries) {
+            textToShow += "\t" + entry.assignmentTitle + " : " + entry.dueDateTimestamp + "\n";
+        }
+    }    
+
+    subpandaRoot.innerHTML = textToShow;
 }
 
-document.addEventListener('load', () => {
-    dumpCache();
-});
+dumpCache();
