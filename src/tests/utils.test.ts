55bed3b57ac7b539f1ab6ed26bd1915e547d8f18
das08 <takeda5955@gmail.com>
2021-10-13 16:18:45 +0900

    add: テストコードの追加

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -31,15 +31,24 @@ expect.extend({
 
 describe("getDaysUntil()", (): void => {
   test("days: 0 <= x <= 1", (): void => {
-    expect(getDaysUntil(1634893200000, 1634911200000)).toBeWithinRange(0, 1);
+    expect(getDaysUntil(1634893200000, 1634911200000)).toBe("due24h");
   });
   test("days: 1 <= x <= 5", (): void => {
-    expect(getDaysUntil(1634893200000, 1635084000000)).toBeWithinRange(1, 5);
+    expect(getDaysUntil(1634893200000, 1635084000000)).toBe("due5d");
   });
   test("days: 5 <= x <= 14", (): void => {
-    expect(getDaysUntil(1634893200000, 1635498000000)).toBeWithinRange(5, 14);
+    expect(getDaysUntil(1634893200000, 1635498000000)).toBe("due14d");
   });
   test("days: 14 <= x", (): void => {
-    expect(getDaysUntil(1634893200000, 1638176400000)).toBeWithinRange(14, 100);
+    expect(getDaysUntil(1634893200000, 1638176400000)).toBe("dueOver14d");
+  });
+  test("days: 14 <= x", (): void => {
+    expect(getDaysUntil(1634893200000, 9999999990000)).toBe("dueOver14d");
+  });
+  test("days: x < 0", (): void => {
+    expect(getDaysUntil(1634893200000, -1000)).toBe("duePassed");
+  });
+  test("days: x < 0", (): void => {
+    expect(getDaysUntil(1634893200000, -1000)).not.toBe("due5d");
   });
 });
