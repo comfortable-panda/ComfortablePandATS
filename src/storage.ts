49de3ad02db1abe656517afe4ec734a461ca905a
das08 <takeda5955@gmail.com>
2021-12-22 14:54:04 +0900

    add: new method for storage

--- src/storage.ts
+++ src/storage.ts
@@ -1,3 +1,11 @@
+function getKeys(): Promise<any> {
+  return new Promise(function (resolve, reject) {
+    chrome.storage.local.get(null, function (keys: any) {
+      resolve(Object.keys(keys));
+    });
+  });
+}
+
 function loadFromLocalStorage(key: string, ifUndefinedType = "array"): Promise<any> {
   const hostName = window.location.hostname;
   return new Promise(function (resolve, reject) {
@@ -24,8 +32,7 @@ function loadFromLocalStorage(key: string, ifUndefinedType = "array"): Promise<a
   });
 }
 
-function loadFromLocalStorage2(key: string): Promise<any> {
-  const hostName = "panda.ecs.kyoto-u.ac.jp";
+function loadFromLocalStorage2(hostName: string, key: string): Promise<any> {
   return new Promise(function (resolve, reject) {
     chrome.storage.local.get(hostName, function (items: any) {
       if (typeof items[hostName] === "undefined" || typeof items[hostName][key] === "undefined") {
@@ -52,4 +59,4 @@ function saveToLocalStorage(key: string, value: any): Promise<any> {
   });
 }
 
-export { loadFromLocalStorage, loadFromLocalStorage2, saveToLocalStorage };
+export { loadFromLocalStorage, loadFromLocalStorage2, saveToLocalStorage, getKeys };
--- src/subsakai.ts
+++ src/subsakai.ts
@@ -1,22 +1,21 @@
 import { Assignment, CourseSiteInfo } from "./model";
-import { loadFromLocalStorage2 } from "./storage";
+import { getKeys, loadFromLocalStorage2 } from "./storage";
 import { convertArrayToAssignment, mergeIntoAssignmentList, sortAssignmentList } from "./utils";
 import { createMiniSakaiGeneralized } from "./minisakai";
 
 const subSakaiRoot = document.querySelector("#sub-sakai");
 
 async function updateSubSakai(root: Element) {
+  const hostname = (await getKeys())[0];
   let mergedAssignmentList: Array<Assignment>;
 
-  const assignmentList = convertArrayToAssignment(await loadFromLocalStorage2("TSkadaiList"));
-  const quizList = convertArrayToAssignment(await loadFromLocalStorage2("TSQuizList"));
-  const assignmentMemoList = convertArrayToAssignment(await loadFromLocalStorage2("TSkadaiMemoList"));
-  const courseIDs = (await loadFromLocalStorage2("TSlectureids")) as Array<CourseSiteInfo>;
+  const assignmentList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "TSkadaiList"));
+  const quizList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "TSQuizList"));
+  const assignmentMemoList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "TSkadaiMemoList"));
+  const courseIDs = (await loadFromLocalStorage2(hostname, "TSlectureids")) as Array<CourseSiteInfo>;
   mergedAssignmentList = mergeIntoAssignmentList(assignmentList, quizList);
   mergedAssignmentList = mergeIntoAssignmentList(mergedAssignmentList, assignmentMemoList);
   mergedAssignmentList = sortAssignmentList(mergedAssignmentList);
-
-  console.log("merged", mergedAssignmentList);
 
   createMiniSakaiGeneralized(root, mergedAssignmentList, courseIDs, true, (rendered) => {
     console.log(rendered);
