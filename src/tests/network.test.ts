729118b41dd52d029fbfd7b182dbdf74a7c67a93
das08 <takeda5955@gmail.com>
2022-01-12 16:39:11 +0900

    add test

--- src/tests/network.test.ts
+++ src/tests/network.test.ts
@@ -4,6 +4,7 @@
 import { getAssignmentByCourseID } from "../network";
 import fs from "fs";
 import { Assignment, AssignmentEntry, CourseSiteInfo } from "../model";
+import * as utils from "../utils";
 
 describe("testapi()", (): void => {
   beforeEach(() => {
@@ -11,15 +12,38 @@ describe("testapi()", (): void => {
     fetch.resetMocks();
   });
 
-  test("api", async (): Promise<void> => {
+  test("assignmentNotClosed", async (): Promise<void> => {
     const jsonObject = JSON.parse(fs.readFileSync(`./src/tests/resources/assignment1.json`, "utf8"));
     //@ts-ignore
     fetch.mockResponseOnce(JSON.stringify(jsonObject));
+
+    // mock time
+    Object.defineProperty(utils, "nowTime", {
+      value: 1668005000000,
+    });
     const a = await getAssignmentByCourseID("", "");
-    const assignmentEntry = new AssignmentEntry("sample1","Sample Assignment1",1668006000, 1668006000, false, false,false);
+    const assignmentEntry = new AssignmentEntry("sample1", "Sample Assignment1", 1668006000, 1668006000, false, false, false);
     assignmentEntry.assignmentDetail = "--------";
     assignmentEntry.assignmentPage = "/portal/site/";
     const assignment = new Assignment(new CourseSiteInfo("", ""), [assignmentEntry], false);
     expect(a).toEqual(assignment);
   });
+
+  test("assignmentClosed", async (): Promise<void> => {
+    const jsonObject = JSON.parse(fs.readFileSync(`./src/tests/resources/assignment1.json`, "utf8"));
+    //@ts-ignore
+    fetch.mockResponseOnce(JSON.stringify(jsonObject));
+
+    // mock time
+    Object.defineProperty(utils, "nowTime", {
+      value: 1668007000000,
+    });
+    const a = await getAssignmentByCourseID("", "");
+    const assignmentEntry = new AssignmentEntry("sample1", "Sample Assignment1", 1668006000, 1668006000, false, false, false);
+    assignmentEntry.assignmentDetail = "--------";
+    assignmentEntry.assignmentPage = "/portal/site/";
+    const assignment = new Assignment(new CourseSiteInfo("", ""), [], false);
+    expect(a).toEqual(assignment);
+  });
+
 });
