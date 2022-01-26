90d2c52dd34e5596fd813f1d301532cafcbb1af2
das08 <takeda5955@gmail.com>
2022-01-26 16:18:40 +0900

    add: test for updating isRead flags

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-import { getDaysUntil } from "../utils";
+import * as utils from "../utils";
+import { Assignment, AssignmentEntry, CourseSiteInfo } from "../model";
 
 // Declare toBeWithinRange as global method.
 declare global {
@@ -20,37 +21,72 @@ expect.extend({
     if (pass) {
       return {
         message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
-        pass: true
+        pass: true,
       };
     } else {
       return {
         message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
-        pass: false
+        pass: false,
       };
     }
-  }
+  },
 });
 
 describe("getDaysUntil()", (): void => {
   test("days: 0 <= x <= 1", (): void => {
-    expect(getDaysUntil(1634893200000, 1634911200000)).toBe("due24h");
+    expect(utils.getDaysUntil(1634893200000, 1634911200000)).toBe("due24h");
   });
   test("days: 1 <= x <= 5", (): void => {
-    expect(getDaysUntil(1634893200000, 1635084000000)).toBe("due5d");
+    expect(utils.getDaysUntil(1634893200000, 1635084000000)).toBe("due5d");
   });
   test("days: 5 <= x <= 14", (): void => {
-    expect(getDaysUntil(1634893200000, 1635498000000)).toBe("due14d");
+    expect(utils.getDaysUntil(1634893200000, 1635498000000)).toBe("due14d");
   });
   test("days: 14 <= x", (): void => {
-    expect(getDaysUntil(1634893200000, 1638176400000)).toBe("dueOver14d");
+    expect(utils.getDaysUntil(1634893200000, 1638176400000)).toBe("dueOver14d");
   });
   test("days: 14 <= x", (): void => {
-    expect(getDaysUntil(1634893200000, 9999999990000)).toBe("dueOver14d");
+    expect(utils.getDaysUntil(1634893200000, 9999999990000)).toBe("dueOver14d");
   });
   test("days: x < 0", (): void => {
-    expect(getDaysUntil(1634893200000, -1000)).toBe("duePassed");
+    expect(utils.getDaysUntil(1634893200000, -1000)).toBe("duePassed");
   });
   test("days: x < 0", (): void => {
-    expect(getDaysUntil(1634893200000, -1000)).not.toBe("due5d");
+    expect(utils.getDaysUntil(1634893200000, -1000)).not.toBe("due5d");
+  });
+});
+
+// jest.mock("../utils", () => {
+//   // 実際のモジュールを取得
+//   const Utils = jest.requireActual("../utils");
+//   return {
+//     // モック化不要なものはそのまま
+//     ...Utils,
+//     // テスト対象のみ置き換える
+//     getSiteCourseID: jest.fn().mockImplementation(() => {
+//       return "keyClass";
+//     }),
+//   };
+// });
+
+describe("updateIsReadFlag()", (): void => {
+  test("doesMatchCourseID", (): void => {
+    const spyGetSiteCourseID = jest.spyOn(utils, "getSiteCourseID").mockImplementation(() => {
+      return "59F7CE3C-5C9A-44A0-963B-E64C0D0A9109";
+    });
+    const sampleAssignmentEntry = new AssignmentEntry("id1", "title", 1000000, 100000, false, false, false);
+    const inputAssignmentList = [
+      new Assignment(
+        new CourseSiteInfo("59F7CE3C-5C9A-44A0-963B-E64C0D0A9109", "course1"),
+        [sampleAssignmentEntry],
+        false
+      ),
+    ];
+    const expectAssignmentList = inputAssignmentList;
+    expectAssignmentList[0].isRead = true;
+
+    const result = utils.updateIsReadFlag(inputAssignmentList);
+    expect(result).toStrictEqual(expectAssignmentList);
+    expect(spyGetSiteCourseID).toHaveBeenCalled();
   });
 });
