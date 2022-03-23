8e3fcc040f83737497f448423b1a583fe05eabed
das08 <41512077+das08@users.noreply.github.com>
2022-03-23 16:07:15 +0900

    add: added release building for firefox

---
+++ tasks/webpack.config.firefox.js
@@ -0,0 +1,18 @@
+const commonConfig = require("../webpack.config.js");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path")
+
+const specificConfig = Object.assign({}, commonConfig);
+
+specificConfig.output = {
+    path: path.resolve(__dirname, '..') + "/dist/source/firefox"
+};
+specificConfig.plugins.push(
+    new CopyPlugin({
+        patterns: [
+            { from: "./manifest.json" }
+        ]
+    })
+);
+
+module.exports = specificConfig;
