4fdc2bfcad2c4b0318660c87d611705df5a3c308
das08 <41512077+das08@users.noreply.github.com>
2022-03-23 16:07:28 +0900

    fix: refactor

--- tasks/webpack.config.chrome.js
+++ tasks/webpack.config.chrome.js
@@ -1,10 +1,11 @@
-const commonConfig = require("./webpack.config.js");
+const commonConfig = require("../webpack.config.js");
 const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path")
 
 const specificConfig = Object.assign({}, commonConfig);
 
 specificConfig.output = {
-    path: __dirname + "/dist/source/chrome"
+    path: path.resolve(__dirname, '..') + "/dist/source/chrome"
 };
 specificConfig.plugins.push(
     new CopyPlugin({
