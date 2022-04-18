9f279f4426263b22b98fc10d222448d696fe09db
das08 <41512077+das08@users.noreply.github.com>
2022-04-18 13:32:55 +0900

    fix: fixed 🐞

--- src/eventListener.ts
+++ src/eventListener.ts
@@ -14,11 +14,13 @@ let toggle = false;
 function toggleMiniSakai(): void {
   if (toggle) {
     // Hide miniSakai
-    miniSakai.style.width = "0px";
+    miniSakai.classList.remove("cs-show");
+    miniSakai.classList.add("cs-hide");
     document.getElementById("cs-cover")?.remove();
   } else {
     // Display miniSakai
-    miniSakai.style.width = "300px";
+    miniSakai.classList.remove("cs-hide");
+    miniSakai.classList.add("cs-show");
     const cover = document.createElement("div");
     cover.id = "cs-cover";
     document.getElementsByTagName("body")[0].appendChild(cover);
