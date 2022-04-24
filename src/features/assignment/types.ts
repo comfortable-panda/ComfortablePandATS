93e005007c2ca0e75790ae1779a6890765616ce1
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-04-25 01:51:10 +0900

    memo

--- src/features/assignment/types.ts
+++ src/features/assignment/types.ts
@@ -2,16 +2,16 @@ import { Renderable } from "../../minisakai";
 import { Course } from "../course/types";
 
 export class AssignmentEntry implements Renderable {
-  constructor(public id: string, public title: string, public dueTime: number|null, public closeTime: number|null, public hasFinished:boolean) {
+  constructor(public id: string, public title: string, public dueTime: number | null, public closeTime: number | null, public hasFinished: boolean) {
   }
 
   render(): [React.Component<{}, {}, any>, number][] {
-      throw "not yet implemented";
+    throw "not yet implemented";
   }
 };
 
 export class Assignment implements Renderable {
-  constructor(public course: Course, public entries:Array<AssignmentEntry>, public isRead:boolean) {}
+  constructor(public course: Course, public entries: Array<AssignmentEntry>, public isRead: boolean) { }
 
   render(): [React.Component<{}, {}, any>, number][] {
     return this.entries.map(e => e.render()).reduce((acc, val) => acc.concat(val), []);
--- src/features/memo/types.ts
+++ src/features/memo/types.ts
@@ -1,14 +1,18 @@
+import { Renderable } from "../../minisakai";
 import { Course } from "../course/types";
 
-export type MemoEntry = {
-  id: string;
-  title: string;
-  dueTime: number | null;
-  hasFinished: boolean;
+export class MemoEntry implements Renderable {
+  constructor(public id: string, public title: string, public dueTime: number | null, public hasFinished: boolean) { }
+
+  render(): [React.Component<{}, {}, any>, number][] {
+    throw "aa";
+  }
 };
 
-export type Memo = {
-  course: Course;
-  entries: Array<MemoEntry>;
-  isRead: boolean;
+export class Memo implements Renderable {
+  constructor(public course: Course, public entries: Array<MemoEntry>, public isRead: boolean) { }
+
+  render(): [React.Component<{}, {}, any>, number][] {
+    return this.entries.map(e => e.render()).reduce((acc, val) => acc.concat(val), []);
+  }
 };
