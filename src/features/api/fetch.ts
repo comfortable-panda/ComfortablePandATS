e33137206aa34702b8b5a4ca7b5ae3847e46f777
das08 <41512077+das08@users.noreply.github.com>
2022-04-25 01:30:20 +0900

    add: fetch

--- src/features/api/fetch.ts
+++ src/features/api/fetch.ts
@@ -4,6 +4,25 @@ import { Course } from "../course/types";
 import { toAssignments } from "../assignment/convert";
 import { toQuizzes } from "../quiz/convert";
 
+/* Sakai のお気に入り欄からCourseを取得する */
+export const fetchCourse = (): Array<Course> => {
+  const elementCollection = document.getElementsByClassName("fav-sites-entry");
+  const elements = Array.prototype.slice.call(elementCollection);
+  const courses: Array<Course> = [];
+  for (const elem of elements) {
+    const name = elem.getElementsByTagName("div")[0].getElementsByTagName("a")[0];
+    const m = name.href.match("(https?://[^/]+)/portal/site-?[a-z]*/([^/]+)");
+    if (m && m[2][0] !== "~") {
+      const course: Course = {
+        id: m[2],
+        name: name.title,
+      };
+      courses.push(course);
+    }
+  }
+  return courses;
+};
+
 /* Sakai APIから課題を取得する */
 export const fetchAssignment = (baseURL: string, courseID: string): Promise<(course: Course) => Assignment> => {
   const queryURL = baseURL + "/direct/assignment/site/" + courseID + ".json";
