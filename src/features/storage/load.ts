f2dc4a34acd8768781d772cff526066401fec369
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 16:38:33 +0900

    fixed undefined key issue

--- src/features/storage/load.ts
+++ src/features/storage/load.ts
@@ -1,7 +1,11 @@
 export const fromStorage = <T>(hostname: string, key: string, decoder: (data: any) => T): Promise<T> => {
   return new Promise(function (resolve) {
     chrome.storage.local.get(hostname, function (items: any) {
-      resolve(decoder(items[hostname][key]));
+      if (hostname in items && key in items[hostname]) {
+        resolve(decoder(items[hostname][key]));
+      } else {
+        resolve(decoder(undefined));
+      }
     });
   });
 };
