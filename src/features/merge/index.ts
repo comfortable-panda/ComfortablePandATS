598cfe65baf294774eb36edfd9c64f224631b63d
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 16:29:14 +0900

    renamed to plural

--- src/features/merge/index.ts
+++ src/features/merge/index.ts
@@ -22,13 +22,13 @@ export const mergeEntities = <Entity extends EntityProtocol>(oldEntities: Array<
 };
 
 export const mergeEntries = <Entry extends EntryProtocol>(oldEntryMap: Map<string, Entry>, newEntryMap: Map<string, Entry>): Array<Entry> => {
-  const entry: Array<Entry> = [];
+  const entries: Array<Entry> = [];
   newEntryMap.forEach((entry, id) => {
     const oldEntry = oldEntryMap.get(id);
     if (oldEntry !== undefined) {
       entry.hasFinished = oldEntry.hasFinished;
     }
-    entry.push(entry);
+    entries.push(entry);
   });
-  return entry;
+  return entries;
 };
