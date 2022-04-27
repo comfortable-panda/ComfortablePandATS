5d9844c77e7b8cb817a9348baf2e077c4b759ce7
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 15:30:37 +0900

    add get method

--- src/features/memo/getMemo.ts
+++ src/features/memo/getMemo.ts
@@ -5,3 +5,7 @@ import { fromStorage } from "../storage/load";
 export const getStoredMemos = (hostname: string): Promise<Array<Memo>> => {
   return fromStorage<Array<Memo>>(hostname, "CS_MemoList", decodeMemoFromArray);
 };
+
+export const getQuizzes = async (hostname: string): Promise<Array<Memo>> => {
+  return await getStoredMemos(hostname);
+};
