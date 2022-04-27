de6d799d793b18b2b4b8e95cdfac642127b9150e
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 13:48:11 +0900

    add tests

---
+++ src/tests/merge.test.ts
@@ -0,0 +1,141 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { Assignment, AssignmentEntry } from "../features/assignment/types";
+import { Course } from "../features/course/types";
+import { mockAssignmentEntry } from "./mock/generator";
+import { mergeAssignment, mergeAssignmentEntry } from "../features/merge";
+import _ from "lodash";
+
+describe("Asssignment", () => {
+  test("Has no old assignment", () => {
+    const oldAssignments: Array<Assignment> = [];
+    const newAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, false),
+          mockAssignmentEntry("a3", 300, 300, false),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+    const expectAssignments = _.cloneDeep(newAssignments);
+    const mergedAssignments = mergeAssignment(oldAssignments, newAssignments);
+    expect(mergedAssignments).toStrictEqual(expectAssignments);
+  });
+
+  test("Old assignment with few entry", () => {
+    const oldAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a2", 200, 200, false),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+
+    const newAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, false),
+          mockAssignmentEntry("a3", 300, 300, false),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+    const expectAssignments = _.cloneDeep(newAssignments);
+    const mergedAssignments = mergeAssignment(oldAssignments, newAssignments);
+    expect(mergedAssignments).toStrictEqual(expectAssignments);
+  });
+
+  test("Old assignment with finished entries", () => {
+    const oldAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, true),
+          mockAssignmentEntry("a3", 300, 300, true),
+        ],
+        false
+      ),
+    ];
+
+    const newAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, false),
+          mockAssignmentEntry("a3", 300, 300, false),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+    const expectAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, true),
+          mockAssignmentEntry("a3", 300, 300, true),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+    const mergedAssignments = mergeAssignment(oldAssignments, newAssignments);
+    expect(mergedAssignments).toStrictEqual(expectAssignments);
+  });
+
+  test("Old assignment with old dueTime & closeTime", () => {
+    const oldAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 100, 100, true),
+          mockAssignmentEntry("a3", 100, 100, true),
+        ],
+        false
+      ),
+    ];
+
+    const newAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, false),
+          mockAssignmentEntry("a3", 300, 300, false),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+    const expectAssignments = [
+      new Assignment(
+        new Course("course1", "course1", ""),
+        [
+          mockAssignmentEntry("a1", 100, 100, false),
+          mockAssignmentEntry("a2", 200, 200, true),
+          mockAssignmentEntry("a3", 300, 300, true),
+          mockAssignmentEntry("a4", 100, 100, false),
+        ],
+        false
+      ),
+    ];
+    const mergedAssignments = mergeAssignment(oldAssignments, newAssignments);
+    expect(mergedAssignments).toStrictEqual(expectAssignments);
+  });
+});
