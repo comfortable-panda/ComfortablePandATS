8d8015e23524fffd643711d3f0e67f805fa4f644
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 15:57:45 +0900

    commented out artifacts

--- src/content_script.ts
+++ src/content_script.ts
@@ -16,6 +16,8 @@ import {
 import { Config, loadConfigs } from "./settings";
 import { Course } from "./features/course/types";
 import { Assignment as NewAssignment, AssignmentEntry } from './features/assignment/types';
+import { Quiz as NewQuiz, QuizEntry } from './features/quiz/types';
+import { Memo as NewMemo, MemoEntry } from './features/quiz/types';
 import { getAssignments } from "./features/assignment/getAssignment";
 import { getQuizzes } from "./features/quiz/getQuiz";
 import { getMemos } from "./features/memo/getMemo";
@@ -33,90 +35,90 @@ export let mergedAssignmentListNoMemo: Array<Assignment>;
  * @param {boolean} useAssignmentCache
  * @param {boolean} useQuizCache
  */
-export async function loadAndMergeAssignmentList(config: Config, courseSiteInfos: Array<CourseSiteInfo>, useAssignmentCache: boolean, useQuizCache: boolean): Promise<Array<Assignment>> {
-  // Load old assignments and quizzes from local storage
-  const oldAssignmentList = convertArrayToAssignment(await loadFromLocalStorage("CS_AssignmentList"));
-  const oldQuizList = convertArrayToAssignment(await loadFromLocalStorage("CS_QuizList"));
-  let newAssignmentList = [];
-  let newQuizList = [];
-
-  // Use cache
-  if (useAssignmentCache) {
-    newAssignmentList = oldAssignmentList;
-  } else {
-    console.log("Fetching assignments...");
-    const pendingList = [];
-    const courses: Array<Course> = [];
-    // Add course sites to fetch-pending list
-    for (const i of courseSiteInfos) {
-      pendingList.push(getAssignmentByCourseID(config.baseURL, i.courseID));
-      //DEBUG:
-      courses.push(new Course(i.courseID, i.courseName, ''));
-    }
-    // Wait until all assignments are fetched
-    const result = await (Promise as any).allSettled(pendingList);
-    for (const k of result) {
-      if (k.status === "fulfilled") newAssignmentList.push(k.value);
-    }
-
-    //DEBUG:
-    // const assignments = await getSakaiAssignments(courses);
-    // console.log(assignments);
-
-    // Update assignment fetch timestamp
-    await saveToLocalStorage("CS_AssignmentFetchTime", nowTime);
-    config.fetchedTime.assignment = nowTime;
-  }
-  // Compare assignments with old ones
-  mergedAssignmentListNoMemo = compareAndMergeAssignmentList(oldAssignmentList, newAssignmentList);
-  mergedAssignmentList = compareAndMergeAssignmentList(oldAssignmentList, newAssignmentList);
-
-  // Use cache
-  if (useQuizCache) {
-    if (typeof oldQuizList !== "undefined") {
-      newQuizList = oldQuizList;
-    }
-  } else {
-    console.log("Fetching quizzes...");
-    const pendingList = [];
-    // Add course sites to fetch-pending list
-    for (const i of courseSiteInfos) {
-      pendingList.push(getQuizFromCourseID(config.baseURL, i.courseID));
-    }
-    // Wait until all quizzes are fetched
-    const result = await (Promise as any).allSettled(pendingList);
-    for (const k of result) {
-      if (k.status === "fulfilled") newQuizList.push(k.value);
-    }
-    // Update assignment fetch timestamp
-    await saveToLocalStorage("CS_QuizFetchTime", nowTime);
-    config.fetchedTime.quiz = nowTime;
-  }
-  // Compare quizzes with old ones
-  const mergedQuizList = compareAndMergeAssignmentList(oldQuizList, newQuizList);
-
-  // Merge assignments and quizzes
-  mergedAssignmentList = mergeIntoAssignmentList(mergedAssignmentList, mergedQuizList);
-
-  // Load memos
-  const memoList = convertArrayToAssignment(await loadFromLocalStorage("CS_MemoList"));
-  // Merge memos
-  mergedAssignmentList = sortAssignmentList(mergeIntoAssignmentList(mergedAssignmentList, memoList));
-
-  // Save assignments and quizzes to local storage
-  await saveToLocalStorage("CS_AssignmentList", mergedAssignmentListNoMemo);
-  await saveToLocalStorage("CS_QuizList", mergedQuizList);
-
-  return mergedAssignmentList;
-}
+// export async function loadAndMergeAssignmentList(config: Config, courseSiteInfos: Array<CourseSiteInfo>, useAssignmentCache: boolean, useQuizCache: boolean): Promise<Array<Assignment>> {
+//   // Load old assignments and quizzes from local storage
+//   const oldAssignmentList = convertArrayToAssignment(await loadFromLocalStorage("CS_AssignmentList"));
+//   const oldQuizList = convertArrayToAssignment(await loadFromLocalStorage("CS_QuizList"));
+//   let newAssignmentList = [];
+//   let newQuizList = [];
+//
+//   // Use cache
+//   if (useAssignmentCache) {
+//     newAssignmentList = oldAssignmentList;
+//   } else {
+//     console.log("Fetching assignments...");
+//     const pendingList = [];
+//     const courses: Array<Course> = [];
+//     // Add course sites to fetch-pending list
+//     for (const i of courseSiteInfos) {
+//       pendingList.push(getAssignmentByCourseID(config.baseURL, i.courseID));
+//       //DEBUG:
+//       courses.push(new Course(i.courseID, i.courseName, ''));
+//     }
+//     // Wait until all assignments are fetched
+//     const result = await (Promise as any).allSettled(pendingList);
+//     for (const k of result) {
+//       if (k.status === "fulfilled") newAssignmentList.push(k.value);
+//     }
+//
+//     //DEBUG:
+//     // const assignments = await getSakaiAssignments(courses);
+//     // console.log(assignments);
+//
+//     // Update assignment fetch timestamp
+//     await saveToLocalStorage("CS_AssignmentFetchTime", nowTime);
+//     config.fetchedTime.assignment = nowTime;
+//   }
+//   // Compare assignments with old ones
+//   mergedAssignmentListNoMemo = compareAndMergeAssignmentList(oldAssignmentList, newAssignmentList);
+//   mergedAssignmentList = compareAndMergeAssignmentList(oldAssignmentList, newAssignmentList);
+//
+//   // Use cache
+//   if (useQuizCache) {
+//     if (typeof oldQuizList !== "undefined") {
+//       newQuizList = oldQuizList;
+//     }
+//   } else {
+//     console.log("Fetching quizzes...");
+//     const pendingList = [];
+//     // Add course sites to fetch-pending list
+//     for (const i of courseSiteInfos) {
+//       pendingList.push(getQuizFromCourseID(config.baseURL, i.courseID));
+//     }
+//     // Wait until all quizzes are fetched
+//     const result = await (Promise as any).allSettled(pendingList);
+//     for (const k of result) {
+//       if (k.status === "fulfilled") newQuizList.push(k.value);
+//     }
+//     // Update assignment fetch timestamp
+//     await saveToLocalStorage("CS_QuizFetchTime", nowTime);
+//     config.fetchedTime.quiz = nowTime;
+//   }
+//   // Compare quizzes with old ones
+//   const mergedQuizList = compareAndMergeAssignmentList(oldQuizList, newQuizList);
+//
+//   // Merge assignments and quizzes
+//   mergedAssignmentList = mergeIntoAssignmentList(mergedAssignmentList, mergedQuizList);
+//
+//   // Load memos
+//   const memoList = convertArrayToAssignment(await loadFromLocalStorage("CS_MemoList"));
+//   // Merge memos
+//   mergedAssignmentList = sortAssignmentList(mergeIntoAssignmentList(mergedAssignmentList, memoList));
+//
+//   // Save assignments and quizzes to local storage
+//   await saveToLocalStorage("CS_AssignmentList", mergedAssignmentListNoMemo);
+//   await saveToLocalStorage("CS_QuizList", mergedQuizList);
+//
+//   return mergedAssignmentList;
+// }
 
 /**
  * Load course site IDs
  */
-async function loadCourseIDList() {
-  courseIDList = getCourseIDList();
-  await saveToLocalStorage("CS_CourseInfo", courseIDList);
-}
+// async function loadCourseIDList() {
+//   courseIDList = getCourseIDList();
+//   await saveToLocalStorage("CS_CourseInfo", courseIDList);
+// }
 
 async function updateReadFlag() {
   const updatedAssignmentList = updateIsReadFlag(mergedAssignmentListNoMemo);
@@ -127,7 +129,7 @@ async function main() {
   if (isLoggedIn()) {
     createMiniSakaiBtn();
     const config = await loadConfigs();
-    await loadCourseIDList();
+    // await loadCourseIDList();
     // mergedAssignmentList = await loadAndMergeAssignmentList(
     //   config,
     //   courseIDList,
@@ -189,9 +191,9 @@ function getCourses(): Array<Course> {
 async function getEntities(courses: Array<Course>) {
   const hostname = window.location.hostname;
   // TODO: 並列化する
-  const assignment = await getAssignments(hostname, courses, false);
-  const quiz = await getQuizzes(hostname, courses, false);
-  const memo = await getMemos(hostname);
+  const assignment: Array<NewAssignment> = await getAssignments(hostname, courses, false);
+  const quiz: Array<NewQuiz> = await getQuizzes(hostname, courses, false);
+  const memo: Array<NewMemo> = await getMemos(hostname);
   return {
     assignment: assignment,
     quiz: quiz,
