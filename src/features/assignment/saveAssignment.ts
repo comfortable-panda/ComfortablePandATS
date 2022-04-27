748e49ccd0bbbc6a2ff8f352c9c8b9371e8c2696
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 23:00:54 +0900

    save single assignment

--- src/features/assignment/saveAssignment.ts
+++ src/features/assignment/saveAssignment.ts
@@ -1,6 +1,23 @@
-import { Assignment } from "./types";
+import { Assignment, AssignmentEntry } from "./types";
 import { toStorage } from "../storage/save";
+import { fromStorage } from "../storage/load";
+import { decodeAssignmentFromArray } from "./decode";
 
 export const saveAssignments = (hostname: string, assignments: Array<Assignment>): Promise<string> => {
   return toStorage(hostname, "CS_AssignmentList", assignments);
 };
+
+export const saveAssignmentEntry = async (hostname: string, changedEntry: AssignmentEntry) => {
+  const assignments = await fromStorage(hostname, "CS_AssignmentList", decodeAssignmentFromArray);
+  LOOP:
+  for (const assignment of assignments) {
+    const entries = assignment.getEntries();
+    for (let i=0;i<entries.length;i++) {
+      if (entries[i].id === changedEntry.id) {
+        entries[i]=changedEntry;
+        break LOOP;
+      }
+    }
+  }
+  saveAssignments(hostname, assignments);
+}
