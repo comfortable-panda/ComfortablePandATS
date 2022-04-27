90870d42d1fc47582cbb550c5e4e327a668e3799
das08 <41512077+das08@users.noreply.github.com>
2022-04-27 14:43:24 +0900

    protocol

--- src/features/entity/type.ts
+++ src/features/entity/type.ts
@@ -1,12 +1,20 @@
+import { Course } from "../course/types";
+
 export interface EntityProtocol {
-  course: {
-    id: string;
-  };
+  course: Course;
   entries: Array<EntryProtocol>;
   isRead: boolean;
+  getCourse(): Course;
   getEntriesMap(): Map<string, EntryProtocol>;
 }
 
 export interface EntryProtocol {
+  id: string;
+  title: string;
+  dueTime: number;
   hasFinished: boolean;
+  getTimestamp(showLateAcceptedEntry: boolean): number;
+  // TODO: Deprecated: Use `getTimestamp()` instead.
+  getDueDate(): number;
+  getID(): string;
 }
