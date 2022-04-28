642ea99afc16c9ce02034d53ecf0c60474db6942
das08 <41512077+das08@users.noreply.github.com>
2022-04-28 15:02:54 +0900

    reformat

--- src/subsakai.ts
+++ src/subsakai.ts
@@ -8,35 +8,35 @@ const subSakaiRoot = document.querySelector("#sub-sakai");
  * Update subSakai to latest info
  */
 async function updateSubSakai(root: Element) {
-  const hostname = await getHostName();
-  let mergedAssignmentList: Array<Assignment>;
+    const hostname = await getHostName();
+    let mergedAssignmentList: Array<Assignment>;
 
-  const assignmentList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "CS_AssignmentList"));
-  const quizList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "CS_QuizList"));
-  const assignmentMemoList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "CS_MemoList"));
-  const courseIDs = (await loadFromLocalStorage2(hostname, "CS_CourseInfo")) as Array<CourseSiteInfo>;
-  mergedAssignmentList = mergeIntoAssignmentList(assignmentList, quizList);
-  mergedAssignmentList = mergeIntoAssignmentList(mergedAssignmentList, assignmentMemoList);
-  mergedAssignmentList = sortAssignmentList(mergedAssignmentList);
+    const assignmentList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "CS_AssignmentList"));
+    const quizList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "CS_QuizList"));
+    const assignmentMemoList = convertArrayToAssignment(await loadFromLocalStorage2(hostname, "CS_MemoList"));
+    const courseIDs = (await loadFromLocalStorage2(hostname, "CS_CourseInfo")) as Array<CourseSiteInfo>;
+    mergedAssignmentList = mergeIntoAssignmentList(assignmentList, quizList);
+    mergedAssignmentList = mergeIntoAssignmentList(mergedAssignmentList, assignmentMemoList);
+    mergedAssignmentList = sortAssignmentList(mergedAssignmentList);
 
-  // FIXME
-  // await createMiniSakaiGeneralized(root, mergedAssignmentList, courseIDs, true, (rendered) => {
-  //   console.log(rendered);
-  //   root.innerHTML = rendered;
-  // });
+    // FIXME
+    // await createMiniSakaiGeneralized(root, mergedAssignmentList, courseIDs, true, (rendered) => {
+    //   console.log(rendered);
+    //   root.innerHTML = rendered;
+    // });
 }
 
 function addSubSakaiToPopup(): Element | null {
-  if (subSakaiRoot == null) return null;
-  return subSakaiRoot;
+    if (subSakaiRoot == null) return null;
+    return subSakaiRoot;
 }
 
 /**
  * Initialize subSakai
  */
 async function initSubSakai() {
-  const root = addSubSakaiToPopup();
-  root && (await updateSubSakai(root));
+    const root = addSubSakaiToPopup();
+    root && (await updateSubSakai(root));
 }
 
 initSubSakai();
