551448ec0a0d65c3162e51d0a48b0fdef2390385
das08 <41512077+das08@users.noreply.github.com>
2022-04-30 11:22:55 +0900

    add test

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -18,21 +18,52 @@ jest.mock("../constant", () => ({
     }
 }));
 
-describe("getFetchTime", () => {
-    test("Has no old assignment", () => {
+describe("getFetchTime(showCompleted)", () => {
+    test("All greater than or equal to currentTime", () => {
         const settings = new Settings();
         settings.miniSakaiOption = {
-            showCompletedEntry: false,
+            showCompletedEntry: true,
             showLateAcceptedEntry: false
         };
-        settings.appInfo.currentTime = 100000;
+        settings.appInfo.currentTime = 100;
         const input: Array<EntryProtocol> = [
             mockAssignmentEntry("id1", 300, 100, false),
             mockAssignmentEntry("id2", 200, 100, false),
             mockAssignmentEntry("id3", 100, 100, false)
         ];
-        console.log("result", settings, MaxTimestamp);
         const output = getClosestTime(settings, input);
         expect(output).toBe(100);
+    });
+
+    test("Some older than currentTime", () => {
+        const settings = new Settings();
+        settings.miniSakaiOption = {
+            showCompletedEntry: true,
+            showLateAcceptedEntry: false
+        };
+        settings.appInfo.currentTime = 100;
+        const input: Array<EntryProtocol> = [
+            mockAssignmentEntry("id1", 300, 100, false),
+            mockAssignmentEntry("id2", 200, 100, false),
+            mockAssignmentEntry("id3", 50, 100, false)
+        ];
+        const output = getClosestTime(settings, input);
+        expect(output).toBe(200);
+    });
+
+    test("All older than currentTime", () => {
+        const settings = new Settings();
+        settings.miniSakaiOption = {
+            showCompletedEntry: true,
+            showLateAcceptedEntry: false
+        };
+        settings.appInfo.currentTime = 500;
+        const input: Array<EntryProtocol> = [
+            mockAssignmentEntry("id1", 300, 100, false),
+            mockAssignmentEntry("id2", 200, 100, false),
+            mockAssignmentEntry("id3", 50, 100, false)
+        ];
+        const output = getClosestTime(settings, input);
+        expect(output).toBe(MaxTimestamp);
     });
 });
