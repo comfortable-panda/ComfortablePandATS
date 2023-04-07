ecfceb5de13933f29165a6dff7fe49c1b5b3266b
walnuts1018 <r.juglans.1018@gmail.com>
2023-04-07 23:31:56 +0900

    名前変更

--- src/features/storage/index.ts
+++ src/features/storage/index.ts
@@ -1,11 +1,11 @@
 import { HostnameStorage } from "../../constant";
 
 export const fromStorage = <T>(hostname: string, key: string, decoder: (data: any) => T): Promise<T> => {
-    key = hostname+"-"+key
+    const storageKey = hostname+"-"+key
     return new Promise(function (resolve) {
-        chrome.storage.sync.get(key, function (items: any) {
-            if (key in items) {
-                resolve(decoder(items[key]));
+        chrome.storage.sync.get(storageKey, function (items: any) {
+            if (storageKey in items) {
+                resolve(decoder(items[storageKey]));
             } else {
                 resolve(decoder(undefined));
             }
@@ -25,16 +25,16 @@ export const loadHostName = (): Promise<string | undefined> => {
 
 export const toStorage = (hostname: string, key: string, value: any): Promise<string> => {
     // items[hostname][key]だと1 Objectあたり8192Bの制限に引っかかるのでhostname-keyの形で分散させる
-    key = hostname+"-"+key
-    const entity: { [key: string]: [value: any] } = {};
-    entity[key] = value;
+    const storageKey = hostname+"-"+key
+    const entity: { [storageKey: string]: [value: any] } = {};
+    entity[storageKey] = value;
     return new Promise(function(resolve) {
-        chrome.storage.sync.get(key, function (items: any) {
-            if (typeof items[key] === "undefined") {
-                items[key] = {};
+        chrome.storage.sync.get(storageKey, function (items: any) {
+            if (typeof items[storageKey] === "undefined") {
+                items[storageKey] = {};
             }
-            items[key] = value;
-            chrome.storage.sync.set({ [key]: items[key] }, () => {
+            items[storageKey] = value;
+            chrome.storage.sync.set({ [storageKey]: items[storageKey] }, () => {
                 resolve("saved");
             });
         });
