85d3d442d6cbf6e06f60ded81d22917fe3a14db8
walnuts1018 <r.juglans.1018@gmail.com>
2023-04-11 19:39:50 +0900

    del debug code

--- src/features/setting/saveSetting.ts
+++ src/features/setting/saveSetting.ts
@@ -3,7 +3,7 @@ import { toStorage } from "../storage";
 import { SettingsStorage, SyncSupportStorage } from "../../constant";
 
 export const saveSettings = (hostname: string, settings: Settings): Promise<string> => {
-    console.log(settings);
+    // console.log(settings);
     toStorage(hostname, SyncSupportStorage, settings.syncSupport, false);
     return toStorage(hostname, SettingsStorage, settings);
 };
