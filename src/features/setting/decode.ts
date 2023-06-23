aca3997bc5c6aab0ba2e10c82beeaf0f708b5cae
das08 <41512077+das08@users.noreply.github.com>
2023-06-23 15:15:24 +0900

    fix: migration issue

--- src/features/setting/decode.ts
+++ src/features/setting/decode.ts
@@ -3,11 +3,15 @@ import { Settings } from "./types";
 export const decodeSettings = (data: any): Settings => {
     const settings = new Settings();
     if (typeof data === "undefined") return settings;
-    settings.appInfo = data.appInfo;
-    settings.fetchTime = data.fetchTime;
-    settings.cacheInterval = data.cacheInterval;
-    settings.miniSakaiOption = data.miniSakaiOption;
-    settings.timeUntilDeadline = data.timeUntilDeadline;
+    // If the data is not valid, return default settings
+    settings.appInfo = data.appInfo ?? settings.appInfo;
+    settings.fetchTime = data.fetchTime ?? settings.fetchTime;
+    settings.cacheInterval = data.cacheInterval ?? settings.cacheInterval;
+    settings.miniSakaiOption = data.miniSakaiOption ?? settings.miniSakaiOption;
+    settings.timeUntilDeadline = data.timeUntilDeadline ?? settings.timeUntilDeadline;
+
+    console.log("data", data);
+    console.log("settings", settings);
     // For backward compatibility
     for (const colorName of Object.getOwnPropertyNames(settings.color)) {
         if (typeof data.color[colorName] !== "undefined") {
